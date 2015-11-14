03fa7434dc5cd1ca4e4eb2bf0402f24d0dc14ba4
Anton Novojilov <andy@essentialkaos.com>
2015-11-14 14:57:18 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+### Redis CLI Monitor
+
+Tiny redis client for renamed MONITOR commands.
+
+#### Installation
+
+````
+go get github.com/essentialkaos/redis-cli-monitor
+````
+
+#### License
+
+[EKOL](https://essentialkaos.com/ekol)
---
+++ redis-cli-monitor.go
@@ -0,0 +1,131 @@
+package main
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                     Copyright (c) 2009-2015 Essential Kaos                         //
+//      Essential Kaos Open Source License <http://essentialkaos.com/ekol?en>         //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	"github.com/essentialkaos/ek/arg"
+	"github.com/essentialkaos/ek/usage"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+const (
+	ARG_HOST    = "H:host"
+	ARG_PORT    = "p:port"
+	ARG_AUTH    = "a:password"
+	ARG_TIMEOUT = "t:timeout"
+	ARG_HELP    = "h:help"
+	ARG_VER     = "v:version"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+var argList map[string]*arg.V = map[string]*arg.V{
+	ARG_HOST:    &arg.V{Value: "127.0.0.1"},
+	ARG_PORT:    &arg.V{Value: "6379"},
+	ARG_TIMEOUT: &arg.V{Type: arg.INT, Value: 3, Min: 1, Max: 300},
+	ARG_AUTH:    &arg.V{},
+	ARG_HELP:    &arg.V{Type: arg.BOOL, Alias: "u:usage"},
+	ARG_VER:     &arg.V{Type: arg.BOOL, Alias: "ver"},
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func main() {
+	args, errs := arg.Parse(argList)
+
+	if len(errs) != 0 {
+		for _, err := range errs {
+			fmt.Println(err.Error())
+		}
+
+		os.Exit(1)
+	}
+
+	if arg.GetB(ARG_VER) == true {
+		showAbout()
+		return
+	}
+
+	if arg.GetB(ARG_HELP) == true || len(args) == 0 {
+		showUsage()
+		return
+	}
+
+	connect(args[0])
+}
+
+func connect(cmd string) {
+	conn, err := net.DialTimeout("tcp", arg.GetS(ARG_HOST)+":"+arg.GetS(ARG_PORT), time.Second*time.Duration(arg.GetI(ARG_TIMEOUT)))
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	defer conn.Close()
+
+	if arg.GetS(ARG_AUTH) != "" {
+		conn.Write([]byte("AUTH " + arg.GetS(ARG_AUTH) + "\n"))
+	}
+
+	conn.Write([]byte(cmd + "\n"))
+	connbuf := bufio.NewReader(conn)
+
+	for {
+		str, err := connbuf.ReadString('\n')
+		if len(str) > 0 {
+			if str == "+OK\r\n" {
+				continue
+			}
+
+			fmt.Printf("%s", str[1:])
+		}
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+	}
+}
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func showUsage() {
+	info := usage.NewInfo("redis-cli-monitor", "command-name")
+
+	info.AddOption(ARG_HOST, "Server hostname", "ip/host")
+	info.AddOption(ARG_PORT, "Server port", "port")
+	info.AddOption(ARG_AUTH, "Password to use when connecting to the server", "password")
+	info.AddOption(ARG_TIMEOUT, "Connection timeout in seconds", "1-300")
+	info.AddOption(ARG_HELP, "Show this help message")
+	info.AddOption(ARG_VER, "Show version")
+
+	info.AddExample("-h 192.168.0.123 -p 6821 -t 15")
+	info.AddExample("-p 12345 -a MySuppaPassword1234")
+
+	info.Render()
+}
+
+func showAbout() {
+	about := &usage.About{
+		App:     "Redis CLI Monitor",
+		Version: "1.0.2",
+		Desc:    "Tiny redis client for renamed MONITOR commands",
+		Year:    2006,
+		Owner:   "ESSENTIAL KAOS",
+		License: "Essential Kaos Open Source License <https://essentialkaos.com/ekol?en>",
+	}
+
+	about.Render()
+}
